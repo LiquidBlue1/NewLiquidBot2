e20050bc269c32326cd17879a628572ba2cc0087
LiquidBlue1 <36345980+LiquidBlue1@users.noreply.github.com>
2018-02-11 21:00:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -149,11 +149,10 @@ client.on("message", async message => {
         var mutereason = message.content.substring(mutereasondelete).split(" "); // deletes the first letters until it reaches the reason
         var mutereason = mutereason.join(" "); // joins the list kickreason into one line
         if (!mutereason) return message.reply("Please indicate a reason for the mute!") // if no reason
-        mutedmember.addRole(mutedrole) //if reason, kick
+        mutedmember.addRole(Muted) //if reason, kick
             .catch(error => message.reply(`Sorry ${message.author} I couldn't mute because of : ${error}`)); //if error, display error
         message.reply(`${mutedmember.user} has been muted by ${message.author} because: ${mutereason}`); // sends a message saying he was kicked
     }
-  "prefix" : "-"
     if (command == "help") { // creates a command *help
         var embedhelpmember = new Discord.RichEmbed() // sets a embed box to the variable embedhelpmember
             .setTitle("**List of Commands**\n") // sets the title to List of Commands
@@ -175,7 +174,7 @@ client.on("message", async message => {
     }
 
     if (command == "info") { // creates the command *info
-        message.channel.send("You can do --help to see all of my commands! If you have any problems with the Discord server, you can contact an administrator! :smile:") // gives u info
+        message.channel.send("You can do -help to see all of my commands! If you have any problems with the Discord server, you can contact an administrator! :smile:") // gives u info
     }
 
 });
