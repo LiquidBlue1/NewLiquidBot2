09e559ecae4c364be3e6311dd7fe199997133fca
LiquidBlue1 <36345980+LiquidBlue1@users.noreply.github.com>
2018-02-12 18:13:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -155,28 +155,5 @@ client.on("message", async message => {
     }
   
 });
-      if (command == "help") { // creates a command *help
-        var embedhelpmember = new Discord.RichEmbed() // sets a embed box to the variable embedhelpmember
-            .setTitle("**List of Commands**\n") // sets the title to List of Commands
-            .addField(" - help", "Displays this message (Correct usage: -help)") // sets the first field to explain the command *help
-            .addField(" - info", "Tells info about myself :grin:") // sets the field information about the command *info
-            .addField(" - ping", "Tests your ping (Correct usage: -ping)") // sets the second field to explain the command *ping
-            .setColor(0xFFA500) // sets the color of the embed box to orange
-            .setFooter("You need help, do you?") // sets the footer to "You need help, do you?"
-        var embedhelpadmin = new Discord.RichEmbed() // sets a embed box to the var embedhelpadmin
-            .setTitle("**List of Admin Commands**\n") // sets the title
-            .addField(" - say", "Makes the bot say whatever you want (Correct usage: *say [message])")
-            .addField(" - mute", "Mutes a desired member with a reason (Coorect usage: *mute @username [reason])") // sets a field
-            .addField(" - unmute", "Unmutes a muted player (Correct usage: *unmute @username)")
-            .addField(" - kick", "Kicks a desired member with a reason (Correct usage: *kick @username [reason])") //sets a field
-            .setColor(0xFF0000) // sets a color
-            .setFooter("Ooo, an admin!") // sets the footer
-        message.channel.send(embedhelpmember); // sends the embed box "embedhelpmember" to the chatif
-        if(message.member.roles.some(r=>["ADMINISTRATOR"].includes(r.name)) ) return message.channel.send(embedhelpadmin); // if member is a botadmin, display this too
-    }
-
-    if (command == "info") { // creates the command *info
-        message.channel.send("You can do -help to see all of my commands! If you have any problems with the Discord server, you can contact an administrator! :smile:") // gives u inf
-  
 
 client.login(config.token);
