3836d39e4b16901209eeb99791c8f44cd0458cfd
LiquidBlue1 <36345980+LiquidBlue1@users.noreply.github.com>
2018-02-12 18:34:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,7 +67,8 @@ client.on("message", async message => {
     const m = await message.channel.send("- kick , This command must be limited to mods and admins. must have a role named Adminstrator to work ");
     const m = await message.channel.send("- soon will be mute and unmute :)");
     const m = await message.channel.send("- ban , This command must be limited to mods and admins. must have a role named Adminstrator to work ");
-  
+  }
+
   if(command === "say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
@@ -173,28 +174,5 @@ client.on("Status Ready 2", () => {
     client.user.setGame("in Test Mode");
 });
 
-const getDefaultChannel = async (guild) => {
-  // get "original" default channel
-  if(guild.channel.has(guild.id))
-    return guild.channels.get(guild.id)
-
-  // Check for a "general" channel, which is often default chat
-  if(guild.channels.exists("name", "#general"))
-    return guild.channels.find("name", "#general");
-  // Now we get into the heavy stuff: first channel in order where the bot can speak
-  // hold on to your hats!
-  return guild.channels
-   .filter(c => c.type === "text" &&
-     c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
-   .sort((a, b) => a.position - b.position ||
-     Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
-   .first();
-}
-
-// This is called as, for instance:
-client.on("guildMemberAdd", member => {
-  const channel = getDefaultChannel(member.guild);
-  channel.send(`Welcome ${member} to the server, wooh!`);
-});
 
 client.login(config.token);
