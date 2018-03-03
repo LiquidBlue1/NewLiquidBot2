97e268db897216d602edfcf641a5d87ae4c543ab
LiquidBlue1 <36345980+LiquidBlue1@users.noreply.github.com>
2018-03-03 13:14:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -131,6 +131,40 @@ client.on("message", async message => {
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
+
+  if(command === prefix + "mute"){
+
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Vous n'avez pas les droits pour muter un utilisateur !");
+
+    let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    if(!toMute) return message.channel.send("Please enter a user !");
+    let role = message.guild.roles.find(r => r.name === "Moved users");
+    if(!role){
+      try {
+        role = await message.guild.createRole({
+          name: "Moved users",
+          color:"#000000",
+          permissions:[]
+        });
+
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(role, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          });
+        });
+      } catch (e) {
+        console.log(e.stack)
+      }
+    }
+
+    if(toMute.roles.has(role.id)) return message.channel.send('This user is already mutated !');
+
+    await(toMute.addRole(role));
+    message.channel.send("I mutated it !");
+
+    return;
+  }
 	
 });
 
